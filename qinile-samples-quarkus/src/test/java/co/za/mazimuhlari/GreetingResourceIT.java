71a5798c3fce9399ba391e9f2a8aac48f56380fc
Mazi Muhlari <mazi.muhlari@gmail.com>
2025-07-20 11:22:14 +0200

    Added sample project

---
+++ qinile-samples-quarkus/src/test/java/co/za/mazimuhlari/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package co.za.mazimuhlari;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
